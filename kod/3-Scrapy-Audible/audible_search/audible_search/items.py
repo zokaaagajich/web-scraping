097b9cbec53f20976d89d3432bfde44e1331c2ed
zokaaa_gajich <zorana.gajic.zg@gmail.com>
2023-08-02 12:05:46 +0200

    Final touches

---
+++ kod/3-Scrapy-Audible/audible_search/audible_search/items.py
@@ -0,0 +1,5 @@
+import scrapy
+
+
+class AudibleSearchItem(scrapy.Item):
+    pass
---
+++ kod/3-Scrapy-Audible/audible_search/audible_search/settings.py
@@ -0,0 +1,6 @@
+BOT_NAME = 'audible_search'
+
+SPIDER_MODULES = ['audible_search.spiders']
+NEWSPIDER_MODULE = 'audible_search.spiders'
+
+ROBOTSTXT_OBEY = True
